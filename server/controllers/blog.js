5daea8ce5f24cc06542662dc1628facea558b1de
Furkan <furkan5590@hotmail.com>
2023-11-11 15:08:22 +0300

    finding blog part was finished

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,31 +1,19 @@
 import Blog from "../models/blog.js";
 
 export const createBlog = async (req, res) => {
-/*   try {
+  console.log(req.body);
+
+  try {
     const { title, content } = req.body;
     const newBlog = new Blog({ title, content });
     const result = await newBlog.save();
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  } */
-
-  console.log(req.body)
-
-  try {
-    const {title ,content} = req.body
-    const newBlog = new Blog({title, content})
-    const result = await newBlog.save()
     res.status(201).json(result);
     console.log("Connected to postBlog");
   } catch (error) {
     console.log("doesnt Connected to postBlog");
     res.status(404).json({
-      message : error.message
-      
-    })
+      message: error.message,
+    });
   }
 };
 
@@ -42,6 +30,21 @@ export const getBlogs = async (req, res) => {
 
 export const getBlog = async (req, res) => {
   // Retrieve a single blog post by ID from the database
+
+  const blogId = req.params.id;
+  try {
+    //const getBlog = await Blog.findById(blogId);
+    const blog = await Blog.findOne({ _id: blogId });
+
+    if (!getBlog) {
+      return res.status(404).json({ message: "Blog post not found" });
+    }
+
+    return res.status(200).json(blog);
+  } catch (error) {
+    console.error('Error fetching blog post:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 export const updateBlog = async (req, res) => {
@@ -49,5 +52,19 @@ export const updateBlog = async (req, res) => {
 };
 
 export const deleteBlog = async (req, res) => {
-  // Handle deleting a blog post by ID
+  const blogId = req.params.id;
+
+  try {
+    // Assume you have a model named Blog and you want to delete a blog post by ID
+    const deletedBlog = await Blog.findByIdAndDelete(blogId);
+
+    if (!deletedBlog) {
+      return res.status(404).json({ message: "Blog post not found" });
+    }
+
+    return res.status(200).json({ message: "Blog post deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,12 +1,19 @@
 import express from "express";
-import {  getBlogs , createBlog } from "../controllers/blog.js";
+import {
+  getBlogs,
+  createBlog,
+  deleteBlog,
+  getBlog,
+} from "../controllers/blog.js";
 
 const router = express.Router();
 
+router.get("/", getBlogs);
 
-router.get("/" , getBlogs)
+router.post("/postBlog", createBlog);
 
-router.post("/postBlog" , createBlog)
+router.delete("/blogs/:id", deleteBlog);
 
+router.get("/blogs/:id", getBlog);
 
 export default router;
